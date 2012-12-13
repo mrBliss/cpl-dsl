da8a52909322c2b190b6845d067f1c8e4cfad455
Glenn Croes <glenncroes@gmail.com>
2012-12-13 17:54:40 +0100

    SQLite DB imports

--- src/globair/TableImport.scala
+++ src/globair/TableImport.scala
@@ -11,14 +11,19 @@ object TableImport extends App {
 	val importData = Source.fromFile(importFileLoc).mkString 
 	println(importData)
 	
+	var queries = importData.split(";");
+	
 	 // Load the SQLite driver
   Class.forName("org.sqlite.JDBC");
 
   val conn = DriverManager.getConnection("jdbc:sqlite:test.db");
   val statement = conn.createStatement()
   statement.setQueryTimeout(30);
-
-  statement.executeUpdate(importData);
+  for(query <- queries){
+	  println(query)
+	  statement.executeUpdate(query)
+  }
+  //statement.executeUpdate(importData);
   
   conn.close();
 }
