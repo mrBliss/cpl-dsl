ae0fda21b2a9e975e82ac62bc8ac108d4bbe6560
Glenn Croes <glenncroes@gmail.com>
2012-12-13 18:24:48 +0100

    Format of TableImport

--- src/globair/TableImport.scala
+++ src/globair/TableImport.scala
@@ -3,27 +3,27 @@ import scala.io.Source
 import java.sql.{ Connection, DriverManager, ResultSet, SQLException, Statement }
 
 object TableImport extends App {
-	
-	val importFileLoc = ".\\analysis\\database.sqlite"
-	
-	println("Importing table file from: " + System.getProperty("user.dir")
-			+ importFileLoc.substring(1,importFileLoc.length()))
-	val importData = Source.fromFile(importFileLoc).mkString 
-	println(importData)
-	
-	var queries = importData.split(";");
-	
-	 // Load the SQLite driver
+
+  val importFileLoc = ".\\analysis\\database.sqlite"
+
+  println("Importing table file from: " + System.getProperty("user.dir")
+    + importFileLoc.substring(1, importFileLoc.length()))
+  val importData = Source.fromFile(importFileLoc).mkString
+  println(importData)
+
+  var queries = importData.split(";");
+
+  // Load the SQLite driver
   Class.forName("org.sqlite.JDBC");
 
   val conn = DriverManager.getConnection("jdbc:sqlite:test.db");
   val statement = conn.createStatement()
   statement.setQueryTimeout(30);
-  for(query <- queries){
-	  println(query)
-	  statement.executeUpdate(query)
+  for (query <- queries) {
+    println(query)
+    statement.executeUpdate(query)
   }
   //statement.executeUpdate(importData);
-  
+
   conn.close();
 }
